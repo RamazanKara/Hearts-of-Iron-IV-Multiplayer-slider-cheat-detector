0c89ece2d68e2895247ccc010d7b0e6ed34ff879
RamazanKara <ramazankara@live.de>
2018-08-19 21:33:50 +0200

    changed the description
    Use this one if you changed the directory of your save games

--- hoi4_buffdetector_withoutpaths.bat
+++ hoi4_buffdetector_withoutpaths.bat
@@ -1,4 +1,4 @@
 @echo off
 for /f %%i in ('findstr /m /c:custom_diff *.hoi4') do set cheats=%%i
-echo these saves have sliderbuffs on: %cheats%
+echo these saves have the sliderbuffs on: %cheats%
 pause
